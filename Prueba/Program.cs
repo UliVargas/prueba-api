bc0253cb43c3cc2694ffbf8c315b2820c8f398d4
Ulises Vargas <uli.vargas02@gmail.com>
2026-04-13 23:27:43 -0600

    cambio swagger

--- Prueba/Program.cs
+++ Prueba/Program.cs
@@ -14,8 +14,6 @@ if (!string.IsNullOrWhiteSpace(port))
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
@@ -29,14 +27,37 @@ using (var scope = app.Services.CreateScope())
 
 // Configure the HTTP request pipeline.
 app.MapOpenApi();
-app.UseSwagger();
-app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapGet("/", () => Results.Ok(new { status = "ok", message = "API viva" }));
+
+app.MapGet("/swagger", () => Results.Redirect("/swagger/index.html"));
+app.MapGet("/swagger/index.html", () => Results.Content(
+    """
+    <!doctype html>
+    <html>
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>API Docs</title>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
+      <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+      <script>
+        window.ui = SwaggerUIBundle({
+          url: '/openapi/v1.json',
+          dom_id: '#swagger-ui'
+        });
+      </script>
+    </body>
+    </html>
+    """,
+    "text/html"));
 
 app.MapControllers();
 
